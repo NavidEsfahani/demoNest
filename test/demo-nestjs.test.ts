4855ea31260f559d88871bb59b8f3d39c3722361
Navid Esfahani <navid.esfahani@oruxa.com>
2023-08-25 17:03:13 -0400

    Initial commit

---
+++ test/demo-nestjs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DemoNestjs from '../lib/demo-nestjs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-nestjs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoNestjs.DemoNestjsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
