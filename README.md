7d5a136e092a5bb210de7bb896d0c6634cca16ad
Navid Esfahani <navid.esfahani@oruxa.com>
2023-08-28 08:15:24 -0400

    initiated

--- README.md
+++ README.md
@@ -12,3 +12,18 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+
+npm install @aws-cdk/aws-lambda @aws-cdk/core
+
+
+My Personal:
+- export AWS_SECRET_ACCESS_KEY=t20lkhvsJZZLNGONGiQQaiV8m+8g1EDqQLrgXEkf
+- export AWS_ACCESS_KEY_ID=AKIAQR3HT2N72CH7WQJL
+
+
+
+BRC:
+- export AWS_SECRET_ACCESS_KEY=3jVcSEcFrbTNN52+0IziXbWiiglmUcd0JFEpotX3
+- export AWS_ACCESS_KEY_ID=AKIAX4364FFXSBE4DBGX
--- lib/demo-nestjs-stack.ts
+++ lib/demo-nestjs-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
-export class DemoNestjsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+export class LambdaNodeCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myFunction = new lambda.Function(this, 'LambdaNodeStack', {
+      code: lambda.Code.fromAsset('./src'),
+      functionName: "lambdaNode",
+      handler: 'index.handler',
+      memorySize: 1024,
+      runtime: lambda.Runtime.NODEJS_14_X,
+      timeout: cdk.Duration.seconds(300),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoNestjsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // Defines an API Gateway REST API resource backed by our "helloWorldFunction" function.
+    new apigateway.LambdaRestApi(this, 'Endpoint', {
+      handler: myFunction
+    });
+    
   }
 }
+
+
+
+
